21dfbec7720bd76ff1f14bd1ab78b373ad305dab
Vinicius Melz <viniciusgmelz@gmail.com>
2024-11-16 17:32:20 -0300

    feat(visualizacaoRelatorio): tela de relatorio finalizada

--- app/src/main/java/com/application/aplicativogerenciadordeveiculos/view/fragments/visualizaRelatorioFragment.java
+++ app/src/main/java/com/application/aplicativogerenciadordeveiculos/view/fragments/visualizaRelatorioFragment.java
@@ -7,10 +7,15 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.application.aplicativogerenciadordeveiculos.R;
 import com.application.aplicativogerenciadordeveiculos.databinding.FragmentVisualizaEntradasBinding;
@@ -20,11 +25,15 @@ import com.application.aplicativogerenciadordeveiculos.view.viewModel.Informacoe
 import com.application.aplicativogerenciadordeveiculos.view.viewModel.VisualizaEntradasViewModel;
 import com.application.aplicativogerenciadordeveiculos.view.viewModel.VisualizaRelatorioViewModel;
 
+import java.text.DecimalFormat;
+
 public class visualizaRelatorioFragment extends Fragment {
 
     private VisualizaRelatorioViewModel mViewModel;
     InformacoesViewModel informacoesViewModel;
     FragmentVisualizaRelatorioBinding binding;
+
+    String valorTotalEntrada, valorTotalSaidas, valorQuilometragem, valorMediaCombustivel;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -38,13 +47,86 @@ public class visualizaRelatorioFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(VisualizaRelatorioViewModel.class);
         informacoesViewModel = new ViewModelProvider(getActivity()).get(InformacoesViewModel.class);
+        mViewModel.setVeiculoSelecionado(informacoesViewModel.getmVeiculoSelecionado().getValue());
+
+        WebSettings webSettings = binding.wvGrafico.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        binding.wvGrafico.setWebViewClient(new WebViewClient());
+
+        DecimalFormat df = new DecimalFormat("0.00");
+        df.setMaximumFractionDigits(2);
+        valorTotalEntrada = df.format(informacoesViewModel.getmVeiculoSelecionado().getValue().getValorTotalEntradas());
+        valorTotalSaidas = df.format(informacoesViewModel.getmVeiculoSelecionado().getValue().getValorTotalSaidas());
+        valorQuilometragem = String.valueOf(informacoesViewModel.getmVeiculoSelecionado().getValue().getQuilometragem());
+        valorMediaCombustivel = df.format(informacoesViewModel.getmVeiculoSelecionado().getValue().getMediaCombustivel());
+
+        binding.tvItemValorEntradas.setText("R$" + valorTotalEntrada.replace(".", ","));
+        binding.tvItemValorSaidas.setText("R$" + valorTotalSaidas.replace(".",","));
+        binding.tvItemQuilometragem.setText(valorQuilometragem + " KM");
+        binding.tvItemMediaCombustivel.setText(valorMediaCombustivel.replace(".",",") + " KM/L");
+
+        carregaGoogleChart(binding.wvGrafico);
+
+        binding.bVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mViewModel.setVeiculoSelecionado(null);
+                informacoesViewModel.setVeiculoSelecionado(null);
+                Navigation.findNavController(view).navigate(R.id.acao_visualizaRelatorioFragment_para_menuPrincipalFragment);
+            }
+        });
+    }
+
+    private void carregaGoogleChart(WebView webView) {
+        String dataJson = "[['Total Entradas', "+ informacoesViewModel.getmVeiculoSelecionado().getValue().getValorTotalEntradas() +"], ['Total Saidas', "+ informacoesViewModel.getmVeiculoSelecionado().getValue().getValorTotalSaidas() +"]]";
+        String htmlData = "<html>\n" +
+                "  <head>\n" +
+                "    <script type=\"text/javascript\" src=\"https://www.gstatic.com/charts/loader.js\"></script>\n" +
+                "    <script type=\"text/javascript\">\n" +
+                "\n" +
+                "      google.charts.load('current', {'packages':['corechart']});\n" +
+                "\n" +
+                "      google.charts.setOnLoadCallback(drawChart);\n" +
+                "\n" +
+                "      function drawChart() {\n" +
+                "\n" +
+                "        var data = new google.visualization.DataTable();\n" +
+                "        data.addColumn('string', 'Topping');\n" +
+                "        data.addColumn('number', 'Slices');\n" +
+                "        if("+ informacoesViewModel.getmVeiculoSelecionado().getValue().getValorTotalEntradas() +" != 0.0 || "+ informacoesViewModel.getmVeiculoSelecionado().getValue().getValorTotalSaidas() +" != 0.0){" +
+                "        data.addRows(" + dataJson + ");\n" +
+                "\n" +
+                "        var options = {'title':'Comparação Entradas/Saídas',\n" +
+                "                       'width':430,\n" +
+                "                       'height':350," +
+                "                       colors:['green','red'],\n" +
+                "                       pieHole: 0.5};\n" +
+                "\n" +
+                "        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));\n" +
+                "        chart.draw(data, options);\n" +
+                "        }else{" +
+                "           var chart = document.getElementById('chart_div');" +
+                "           chart.style = 'text-align: center; padding-top: 10px; font-weight: bold;';" +
+                "           chart.innerText = 'SEM ENTRADAS E SAÍDAS REGISTRADAS! CADASTRE ALGUMA ENTRADA OU SAÍDA PARA VISUALIZAR O GRÁFICO!'" +
+                "        }" +
+                "      }\n" +
+                "    </script>\n" +
+                "  </head>\n" +
+                "\n" +
+                "  <body style='overflow: hidden;'>\n" +
+                "    <div id=\"chart_div\" style='overflow: hidden; background-color: #D3D3D3'></div>\n" +
+                "  </body>\n" +
+                "</html>";
+
+        webView.loadDataWithBaseURL(null, htmlData, "text/html", "UTF-8", null);
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
-        //mViewModel.limpaEstado();
+        mViewModel.limpaEstado();
     }
 
     @Override
--- app/src/main/java/com/application/aplicativogerenciadordeveiculos/view/viewModel/VisualizaRelatorioViewModel.java
+++ app/src/main/java/com/application/aplicativogerenciadordeveiculos/view/viewModel/VisualizaRelatorioViewModel.java
@@ -1,7 +1,26 @@
 package com.application.aplicativogerenciadordeveiculos.view.viewModel;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.application.aplicativogerenciadordeveiculos.model.Veiculo;
+
 public class VisualizaRelatorioViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    private MutableLiveData<Veiculo> mVeiculoSelecionado;
+
+    public VisualizaRelatorioViewModel() {
+        this.mVeiculoSelecionado = new MutableLiveData<>();
+    }
+
+    public void limpaEstado(){
+        this.mVeiculoSelecionado = new MutableLiveData<>();
+    }
+
+    public MutableLiveData<Veiculo> getmVeiculoSelecionado() {
+        return mVeiculoSelecionado;
+    }
+
+    public void setVeiculoSelecionado(Veiculo veiculo) {
+        this.mVeiculoSelecionado.postValue(veiculo);
+    }
 }
